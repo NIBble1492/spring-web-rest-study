20f75d39ae1ff1fde26a495ede947867237bacce
JUNHYEONG LEE <nibble1492@gmail.com>
2026-05-20 14:26:54 +0900

    refactor: RsData에 제네릭 도입 및 컨트롤러 적용 - RsData<T>로 변경하여 타입 안정성 확보

--- src/main/java/com/back/domain/post/postComment/controller/ApiV1PostCommentController.java
+++ src/main/java/com/back/domain/post/postComment/controller/ApiV1PostCommentController.java
@@ -47,7 +47,7 @@ public class ApiV1PostCommentController {
 
     @Transactional
     @GetMapping("/{id}/delete")
-    public RsData delete(
+    public RsData<PostCommentDto> delete(
             @PathVariable int postId,
             @PathVariable int id
     ) {
@@ -56,7 +56,7 @@ public class ApiV1PostCommentController {
 
         postService.deleteComment(post, postComment);
 
-        return new RsData(
+        return new RsData<>(
                 "200-1",
                 "%d번 댓글이 삭제되었습니다.".formatted(id),
                 new PostCommentDto(postComment)
