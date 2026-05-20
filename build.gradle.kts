ee3c0e297dbfcebce9c4cf62c8ecb09b75ba6c53
JUNHYEONG LEE <nibble1492@gmail.com>
2026-05-20 09:30:23 +0900

    feat : 게시글 및 댓글 도메인 기본 구현 및 프로젝트 초기 설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,6 +27,12 @@ dependencies {
     testCompileOnly("org.projectlombok:lombok")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     testAnnotationProcessor("org.projectlombok:lombok")
+    implementation("org.springframework.boot:spring-boot-h2console")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    runtimeOnly("com.h2database:h2")
+    testImplementation("org.springframework.boot:spring-boot-starter-data-jpa-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-validation-test")
 }
 
 tasks.withType<Test> {
