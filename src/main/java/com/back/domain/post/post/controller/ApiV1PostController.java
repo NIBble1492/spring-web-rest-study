914cab30ac77e41ffe749888e1b4269fe51be028
JUNHYEONG LEE <nibble1492@gmail.com>
2026-05-21 11:20:22 +0900

    refactor: 게시글 작성 API 요청 및 응답 바디 구조화 - PostWriteReqBody와 PostWriteResBody 정의 및 적용

--- src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
@@ -37,25 +37,38 @@ public class ApiV1PostController {
 
         return new PostDto(post);
     }
-    public record PostWriteFrom (
+    public record PostWriteReqBody (
             @NotBlank
             @Size(min = 2, max = 100)
             String title,
             @NotBlank
-            @Size(min = 2, max = 100)
+            @Size(min = 2, max = 5000)
             String content
+    ) {
+    }
+
+    public record PostWriteResBody(
+            long totalCount,
+            PostDto post
     ) {
     }
 
     @PostMapping
     @Transactional
-    public RsData<PostDto> write(@Valid @RequestBody PostWriteFrom from) {
-        Post post = postService.write(from.title, from.content);
+    public RsData<PostWriteResBody> write(
+            @RequestBody @Valid ApiV1PostController.PostWriteReqBody form
+    ) {
+        Post post = postService.write(form.title, form.content);
+
+        long totalCount = postService.count();
 
         return new RsData<>(
                 "200-1",
                 "%d번 글이 생성되었습니다.".formatted(post.getId()),
-                new PostDto(post)
+                new PostWriteResBody(
+                        totalCount,
+                        new PostDto(post)
+                )
         );
     }
 
