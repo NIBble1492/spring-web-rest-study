510ce0c0686cc43acf7205104f732d729a7260e1
JUNHYEONG LEE <nibble1492@gmail.com>
2026-05-21 14:34:29 +0900

    test: 게시글 등록 API 테스트 코드 추가 및 BackApplicationTests 삭제

---
+++ src/test/java/com/back/domain/post/postComment/controller/ApiV1PostCommentControllerTest.java
@@ -0,0 +1,9 @@
+package com.back.domain.post.postComment.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ApiV1PostCommentControllerTest {
+
+}
