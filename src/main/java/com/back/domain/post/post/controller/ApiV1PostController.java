1c9636f53c2e144abbf111cd415a96941117b95b
JUNHYEONG LEE <nibble1492@gmail.com>
2026-05-21 12:06:46 +0900

    feat: Spring AOP를 이용한 RsData 상태 코드 자동 설정 구현 - ResponseAspect 추가, RsData에 statusCode 필드 추가 및 게시글 등록 API(201) 적용

--- src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
@@ -63,7 +63,7 @@ public class ApiV1PostController {
         long totalCount = postService.count();
 
         return new RsData<>(
-                "200-1",
+                "201-1",
                 "%d번 글이 생성되었습니다.".formatted(post.getId()),
                 new PostWriteResBody(
                         totalCount,
